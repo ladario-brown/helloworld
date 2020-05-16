e1ea433adf6cab2da018017907c3a0e25940e664
Ladario Brown <oracle.lbrown@gmail.com>
2020-05-16 16:03:59 -0400

    Initial Commit

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+
+import os
+os.system ('clear')
+
+'''
+Inspiring Software Engineer
+16 May 2020
+This Hello World code will be the first code I've written in Python. 
+I will be using it to practice Git via Command Prompts. 
+Check out my Git Hub account @https://github.com/ladario-brown
+'''
+
+print ("Hello World") 
